a5208850c8d9dfab8a521158c62bb79765799fb0
ymh <suneverset@naver.com>
2021-01-20 15:41:16 +0900

    def main fixed a lot haha

--- baseball_game.py
+++ baseball_game.py
@@ -280,23 +280,48 @@ def is_no(one_more_input):
 
 def main():
     print("Play Baseball")
-    user_input = 999
-    random_number = str(247)#get_not_duplicated_three_digit_number())
+    random_number = str(get_not_duplicated_three_digit_number())
     print("Random Number is : ", random_number)
-    # ===Modify codes below=============
-    #위의 코드를 포함하여 자유로운 수정이 가능함
+    success = 0
+    more_game = 0
     while True:
-        user_number = input('중복되지 않는 세 자리 숫자를 입력하세요: ')
-        if is_validated_number(user_number):
-            strike, ball = get_strikes_or_ball(user_number, random_number)
-            print(f'{strike:^5d} 스트라이크 {ball:^5d} 볼입니다!')
+        user_input = input('Input guess number : ')
+        if user_input == '0':
+            break
+        elif is_validated_number(user_input) == False:
+            print('Wrong input, Input again')
+            continue
+        strike, ball = get_strikes_or_ball(user_input, random_number)
+        if strike == 3 and ball == 0:
+            success = 1
             break
         else:
-            print('잘못된 숫자/문자를 입력하셨네요!')
-            continue
-    # ==================================
-    print("Thank you for using this program")
-    print("End of the Game")
+            print(f'Strikes: {strike}, Balls: {ball}')
+            
+    if success == 1:
+        while True:
+            one_more = input('You win, one more(Y/N) ?')
+            if is_yes(one_more):
+                # Y, Yes를 입력한 경우
+                more_game = 1
+                break
+            else:
+                # n, no를 입력한 경우
+                if is_no(one_more):
+                    print("Thank you for using this program")
+                    print("End of the Game")
+                    return
+                # 아예 이상한걸 입력한 경우
+                else:
+                    print('Wrong Input, Input again')
+                    continue
+    
+    if more_game == 1:
+        return main()
+    else:
+        print("Thank you for using this program")
+        print("End of the Game")
+
 
 if __name__ == "__main__":
     main()
